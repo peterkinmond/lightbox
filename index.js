1a2bdc2848869aa396b756a41851019641a7e661
Peter Kinmond <peterkinmond@gmail.com>
2016-02-20 15:47:43 -0800

    Display image thumbnails in grid on page
    * Calls Flickr API using XHR and returns JSON payload
    * Thumbnails using "background-size: cover" so that each one is a square

---
+++ index.js
@@ -0,0 +1,38 @@
+var flickrUrl = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=02463078aaf5cf079bb47c1745a278f2&photoset_id=72157647299892609&user_id=41497208%40N04&per_page=20&page=1&media=photos&format=json&nojsoncallback=1";
+var images = []; // Array of all images shown in grid
+
+callImageApi(flickrUrl);
+
+function callImageApi(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", imageApiCallback, false);
+  xhr.open('GET', url, true);
+  xhr.send();
+}
+
+function imageApiCallback() {
+  var data = JSON.parse(this.responseText);
+  images = data.photoset.photo; // Flickr calls the array of photos "photo"
+  displayImages(images);
+}
+
+function displayImages(images) {
+  var imagesDiv = document.getElementById('images');
+  images.forEach(function(image, index) {
+    var imageContainer = document.createElement('div');
+    imageContainer.style.backgroundImage = "url(" + getImageUrl(index, true) + ")";
+    imageContainer.className = "image-container";
+    imageContainer.addEventListener('click', function() { loadLightbox(index); }, false);
+    imageContainer.dataset.index = index;
+    imagesDiv.appendChild(imageContainer);
+  });
+}
+
+function getImageUrl(imageIndex, useThumbnailVersion) {
+  var image = images[imageIndex];
+  var flickrThumbnailId = "q"; // Flickr uses "q" suffix for thumbnail size
+  var flickrLargeSizeId = "c"; // Flickr uses "c" suffic for large size
+  var size = (useThumbnailVersion === true) ? flickrThumbnailId : flickrLargeSizeId;
+  return "https://farm" + image.farm + 
+    ".staticflickr.com/" + image.server + "/" + image.id + "_" + image.secret + "_" + size + ".jpg";
+}
