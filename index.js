8740c0a12f249cc55fa5e66c9b4ad1fc96bcb16c
Peter Kinmond <peterkinmond@gmail.com>
2016-02-20 19:00:39 -0800

    Add previous/next buttons to lightbox
    * Buttons only show if there is a previous/next option
    * Consider making buttons larger for better UX

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var flickrUrl = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=02463078aaf5cf079bb47c1745a278f2&photoset_id=72157647299892609&user_id=41497208%40N04&per_page=20&page=1&media=photos&format=json&nojsoncallback=1",
   images = [], // Array of all images shown in grid
+  currentImageIndex = 0, // Index of image in lightbox
   imagesDiv = document.getElementById('images'),
   lightbox = document.getElementsByClassName('lightbox')[0],
   lightboxImage = document.getElementsByClassName('lightbox-image')[0],
+  prevArrow = document.getElementsByClassName('prev-arrow')[0],
+  nextArrow = document.getElementsByClassName('next-arrow')[0],
   lightboxBackground = document.getElementsByClassName('lightbox-background')[0];
 
-callImageApi(flickrUrl);
+setupEventListeners();
+callImageApi(flickrUrl);  
+
+function setupEventListeners() {
+  prevArrow.addEventListener('click', loadPrevImage, false);
+  nextArrow.addEventListener('click', loadNextImage, false);
+}
 
 function callImageApi(url) {
   var xhr = new XMLHttpRequest();
@@ -31,10 +40,22 @@ function displayImages(images) {
   });
 }
 
+function loadPrevImage() {
+  loadLightbox(currentImageIndex - 1);
+}
+
+function loadNextImage() {
+  loadLightbox(currentImageIndex + 1);
+}
+
 function loadLightbox(index) {
+  currentImageIndex = index;
   lightbox.style.display = "block";
   lightboxBackground.style.display = "block";
   lightboxImage.src = getImageUrl(index, false);
+
+  prevArrow.style.display = (index > 0) ? "block" : "none";
+  nextArrow.style.display = (index < images.length - 1) ? "block" : "none";
 }
 
 function getImageUrl(imageIndex, useThumbnailVersion) {
