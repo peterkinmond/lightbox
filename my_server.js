c01c738e7ea5879c4bedb36ad88f30b1deb95857
Peter Kinmond <peterkinmond@gmail.com>
2016-03-03 22:30:16 -0800

    Get dynamically assigned Heroku port

--- my_server.js
+++ my_server.js
@@ -1,6 +1,9 @@
 var express = require('express'),
     app = express();
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8080);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
